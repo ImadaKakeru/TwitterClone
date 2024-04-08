b048e22b54b1cb4d605e832344e1612fc4c26386
imada kakeru <imada@fun.bio.keio.ac.jp>
2024-04-08 21:02:02 +0900

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Kakeru Imada on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
